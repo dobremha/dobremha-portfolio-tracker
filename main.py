fce77fd629d7a72b9bb6eea494fdbaabecea9b03
dobremha <108837852+dobremha@users.noreply.github.com>
2023-09-02 13:23:59 +0000

    v2

--- main.py
+++ main.py
@@ -5,13 +5,37 @@ import json
 
 app = Flask(__name__)
 
+COINS = [
+  {
+    'id': 1,
+    'description': 'Cardano',
+    'coin': 'ADA',
+    'price': 0.25,
+    'holdings_usdt': 100,
+    'holdings_asset': 2000,
+    'avg_puy_price': 0.29,
+    'p_l_usdt': -22,
+    'p_l_proc': '22%'
+  },
+  {
+    'id': 2,
+    'description': 'Polygon',
+    'coin': 'MATIC',
+    'price': 5.25,
+    'holdings_usdt': 200,
+    'holdings_asset': 1000,
+    'avg_puy_price': 5.11,
+    'p_l_usdt': 11,
+    'p_l_proc': '11%'
+  }
+]
 
 @app.route('/')
 def homepage():
   return render_template("basic.html",
                          titlu_pagina="dobremha-coins",
                          wallet="SAFEPAL",
-                         user_is_authenticated=False)
+                         coins=COINS)
 
 
 if __name__ == "__main__":
